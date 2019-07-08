31075a5038b47020b75230bf0e704b8fc2ab889d
lingardo <lingardo.soul@gmail.com>
2019-07-08 17:09:02 +0300

    Course Project - The Basics

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingradient } from '../shared/ingradient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingradients: Ingradient[] = [
+    new Ingradient('Apples', 5),
+    new Ingradient('Tomatoes', 10),
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
