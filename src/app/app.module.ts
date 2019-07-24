b66c2bb4e76efb6c1f917e8ba0dc664ed687134d
lingardo <lingardo.soul@gmail.com>
2019-07-24 16:17:06 +0300

    Dynamic Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,8 @@ import { AuthComponent } from './auth/auth.component';
 import { environment } from '../environments/environment';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 
 @NgModule({
@@ -41,7 +43,9 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     RecipeStartComponent,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
@@ -62,6 +66,7 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
         multi: true
       }
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
+    entryComponents: [AlertComponent]
   })
   export class AppModule {}
